509c837805e9c4b33533ca94a13683c917775707
zmedailleu <zacharymedailleu@gmail.com>
2026-03-01 12:19:18 -0500

    chore: added function that returns years served in both house and senate for a certain year

--- shared/getmembers.js
+++ shared/getmembers.js
@@ -5,10 +5,29 @@ export function getMembersFromYear(chamber, year, data) {
     return data.filter(member => {
         let servedYear = false;
         member.terms.forEach(term => {
-            if (term.chamber === chamber && term.congress === congressNumberThatYear) {
+            if ((term.chamber === chamber || chamber === "Both") && term.congress === congressNumberThatYear) {
                 servedYear = true;
             }
         })
         return servedYear;
+    })
+}
+
+//returns a list of the years served in the house and senate for each congress member
+export function getYearsServed(year, data) {
+    const congressNumberThatYear = Math.ceil((year - 1788) / 2);
+    return data.map(member => {
+        let houseYears = 0;
+        let senateYears = 0;
+
+        member.terms.forEach(term => {
+            if (term.congress <= congressNumberThatYear) {
+                let yearsServedThatTerm = term.endYear - term.startYear;
+                term.chamber === "Senate" ? senateYears += yearsServedThatTerm : houseYears += yearsServedThatTerm;
+            }
+        })
+
+        //list position 2 is house, position 3 is senate
+        return [member.name, member.yob, houseYears, senateYears]
     })
 }
