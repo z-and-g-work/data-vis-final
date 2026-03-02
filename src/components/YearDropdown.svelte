8a38f7398d7e23806b469c3c8a85af6577443b41
zmedailleu <zacharymedailleu@gmail.com>
2026-03-02 15:11:45 -0500

    test: changing graph by year

---
+++ src/components/YearDropdown.svelte
@@ -0,0 +1,17 @@
+<script>
+    import AgeComparisonChart from "./AgeComparisonChart.svelte";
+
+    let selectedYear = 2024;
+    let options = [];
+    for (let i = 1995; i < 2025; i++) {
+        options.push(i)
+    }
+</script>
+
+<select bind:value={selectedYear}>
+    {#each options as option}
+        <option value={option}>{option}</option>
+    {/each}
+</select>
+
+<AgeComparisonChart {selectedYear} />
