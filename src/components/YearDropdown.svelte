377987c6fceaa5eb2ae62e9913677e5708560666
Gabriel_O <55938114+gabrielolafs@users.noreply.github.com>
2026-03-04 16:59:52 -0500

    feat: timeline

--- src/components/YearDropdown.svelte
+++ src/components/YearDropdown.svelte
@@ -3,8 +3,7 @@
     import AgeComparisonChart from "./AgeComparisonChart.svelte";
     import { getMembersFromYear } from "../shared/getmembers.js";
     import { selectedYear, congressData, loadYear } from "./../shared/dataManager.js";
-
-    onMount(() => loadYear(2024));
+    import "../styles/timeline.css";
 
     let options = [];
 
@@ -23,8 +22,17 @@
 
 </script>
 
-<select bind:value={ $selectedYear }>
-    {#each options as option}
-        <option value={option}>{option}</option>
-    {/each}
-</select>
+<div class="timeline-and-text-container">
+    <p> Currently { $selectedYear } : { $selectedYear % 2 ? '1st' : '2nd' } half of congress { Math.ceil(( $selectedYear - 1788) / 2)} </p>
+    <div class="timeline-container">
+        {#each options as option}
+            <button 
+                type="button"
+                class:active={$selectedYear === option}
+                on:click={() => selectedYear.set(option)}
+            >
+                {option.toString().substring(2, 4)}
+            </button>
+        {/each}
+    </div>
+</div>
