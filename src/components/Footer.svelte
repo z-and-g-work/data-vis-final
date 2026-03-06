5c4940a54f5beff75d975af3b506219cb1bf3ab9
Ben Perry <famogr.perry@gmail.com>
2026-03-06 09:40:12 -0500

    Added demo video
    Added the demo video and embedding the link onto the footer.

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -5,10 +5,13 @@
 
     <p>Data sourced from <a href="https://api.congress.gov" target="_blank" rel="noopener">api.congress.gov</a> · U.S. Congress</p>
     <p>Download our process book <a href="./processbook.pdf" download="Politicians: With Age Comes Experience Process Book">here</a></p>
-<!--    <p>Site Demonstration:</p>-->
-<!--    <iframe-->
-<!--            width = "420"-->
-<!--            height = "236"-->
-<!--            src="INSERT VIDEO LINK HERE, ALSO ADD IT IN THE README">-->
-<!--    </iframe>-->
+    <p>Site Demonstration:</p>
+    <iframe
+        width="420"
+        height="236"
+        src="https://www.youtube.com/embed/a6yR-bWwRNw"
+        title="Site Demonstration"
+        frameborder="0"
+        allowfullscreen>
+    </iframe>
 </footer>
